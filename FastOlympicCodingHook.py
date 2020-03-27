418316498ec952a454ea1b1ce13c1fc4fd523004
Nayeemul Islam Swad <drswad2013@gmail.com>
2020-03-27 16:23:02 +0600

    Add files via upload

---
+++ FastOlympicCodingHook.py
@@ -0,0 +1,49 @@
+import sublime
+import sublime_plugin
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import _thread
+import threading
+
+
+def MakeHandlerClassFromFilename(filename):
+    class HandleRequests(BaseHTTPRequestHandler):
+        def do_POST(self):
+            try:
+                content_length = int(self.headers['Content-Length'])
+                body = self.rfile.read(content_length)
+                tests = json.loads(body.decode('utf8'))
+                tests = tests["tests"]
+                ntests = []
+                for test in tests:
+                    ntest = {
+                        "test": test["input"],
+                        "correct_answers": [test["output"].strip()]
+                    }
+                    ntests.append(ntest)
+                print(filename)
+                with open(filename + ":tests", "w") as f:
+                    f.write(json.dumps(ntests))
+            except Exception as e:
+                print("Error handling POST - " + str(e))
+            threading.Thread(target=self.server.shutdown, daemon=True).start()
+    return HandleRequests
+
+
+class CompetitiveCompanionServer:
+    def startServer(filename):
+        host = 'localhost'
+        port = 12345
+        HandlerClass = MakeHandlerClassFromFilename(filename)
+        httpd = HTTPServer((host, port), HandlerClass)
+        httpd.serve_forever()
+        print("Server has been shutdown")
+
+
+class FastOlympicCodingHookCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        try:
+            _thread.start_new_thread(CompetitiveCompanionServer.startServer,
+                                     (self.view.file_name(),))
+        except Exception as e:
+            print("Error: unable to start thread - " + str(e))
